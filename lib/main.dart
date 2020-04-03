86800771501320e077921fe9b710ee87a137d4d3
Saran <m.saran7410@gmail.com>
2020-04-03 18:41:30 +0530

    Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,61 @@
-import 'package:toast/toast.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
-      title:"App Title",
-      home:Scaffold(
-        appBar: AppBar(
-          title:Text("AppBar")
-        ),
-        body:listView(),
-      )
-    )
+  runApp(MaterialApp(
+    title:"Basics",
+    home: MyApp()
+  )
   );
 }
 
-List<String> Generate(){
-  var items=List<String>.generate(1000, (counter)=>"Item $counter");
-return items;
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+  
 }
 
-Widget listView(){
- var items=Generate();
+class _MyAppState extends State<MyApp>{
+String valu="Select a City";
 
- var list=ListView.builder(
-   itemBuilder:(context,index){
-     return ListTile(
-       leading:Icon(Icons.airport_shuttle),
-       title:Text(items[index]),
-       onTap: (){
-         debugPrint("${items[index]} was clicked");
-         Toast.show("${items[index]} was clicked",context,gravity: Toast.BOTTOM);
-       },
-     );
-   });
-   return list;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("First Page")
+      ),
+      body:Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+        children: <Widget>[
+          TextField(
+            style: TextStyle(
+              fontSize: 25
+            ),
+            onSubmitted: (String inp){
+              setState(() {
+                if(inp==""){
+                  valu= "Cant't Leave it empty";
+                }
+                else{
+                  valu="The city $inp is Available";
+                }
+              });
+            }
+          ),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child:Text(valu),
+            )
+          
+        ],
+      )
+      )
+    );
+  }
+  
+
 }
